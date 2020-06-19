e2749a511a4aaf7d57eb16f2d28634216739b06e
Louis Thabethe <lthabeth@c5r4s10.wethinkcode.co.za>
2020-06-19 14:26:45 +0200

    exercises for day02

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: lthabeth <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 13:28:28 by lthabeth          #+#    #+#             */
-/*   Updated: 2020/06/19 13:35:41 by lthabeth         ###   ########.fr       */
+/*   Updated: 2020/06/19 14:24:23 by lthabeth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,20 @@ void    ft_putchar(char c)
 {
         write(1, &c, 1);
 }
-void    ft_print_alphabet(void)
+
+void    ft_print_reverse_alphabet(void)
 {
-        char    flip(char alphabet);
-        alphabet = 'a';
-                while  (alphabet <= 'z')
+        char   alphabet;
+        alphabet = 'z';
+					while  (alphabet >= 'a')
                 {
                         ft_putchar(alphabet);
-                        alphabet++;
-                }
-ft_putchar('\n');
+                        alphabet--;
+}					
 }
 
 int main()
 {
- ft_print_alphabet();
+ ft_print_reverse_alphabet();
+ return(0);
 }
