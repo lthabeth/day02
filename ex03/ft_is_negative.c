fbdd4cd135fb4e649d640fd2835f69b591fd81a2
Louis Thabethe <lthabeth@c5r4s10.wethinkcode.co.za>
2020-06-25 10:52:28 +0200

    exercises for day02

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -6,7 +6,7 @@
 /*   By: lthabeth <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 16:30:15 by lthabeth          #+#    #+#             */
-/*   Updated: 2020/06/19 17:21:00 by lthabeth         ###   ########.fr       */
+/*   Updated: 2020/06/25 10:49:26 by lthabeth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -22,11 +22,12 @@ void ft_is_negative(int n)
 	} else if (n < '0')
 	{
 		ft_putchar('N');
+		n = -n;
 	}
 }
 int		main()
 {
     
-	ft_is_negative(5);
+	ft_is_negative(42);
 	return(0);
 }
